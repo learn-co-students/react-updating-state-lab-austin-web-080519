735b763c438e87a5a39c994f4b9963108184852b
Anais Ahmed <anaismahmed@uchicago.edu>
2019-10-08 06:01:16 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor(){
+        super()
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    increment = () => {
+        let clicked = this.state.timesClicked;
+        this.setState({
+            timesClicked: ++clicked
+        })
+    }
+
+    render(){
+        return (
+            <button onClick={this.increment}>{this.state.timesClicked}</button>
+        )
+    }
+}
