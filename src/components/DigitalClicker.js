63eed0726db259c9e63f2ef8296a5448d16d15a8
Cody <lawdermiltcody@gmail.com>
2019-10-10 20:28:03 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,24 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+class DigitalClicker extends Component {
+    constructor() {
+        super();
+        this.state = {timesClicked: 0};
+    }
+
+    handleClick = () => {
+        let currentClicks = this.state.timesClicked;
+        currentClicks += 1;
+        this.setState( { timesClicked: currentClicks } );
+    }
+    render() { 
+        return ( 
+            <div>
+                <button onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+         );
+    }
+}
+ 
+export default DigitalClicker;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,43 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+class YouTubeDeBugger extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+            bitrate: 8,
+            video: {
+                resolution: '1080p'
+                }
+            }
+        }
+    };
+
+    changeBitrate = () => {
+        let newBitrate = this.state.settings.bitrate;
+        newBitrate = 12;
+        this.setState( {settings: {
+            ...this.state.settings, bitrate: newBitrate
+        }})
+    }
+
+    changeResolution = () => {
+        let newResolution = this.state.settings.video.resolution;
+        newResolution = '720p';
+        this.setState({ settings: {...this.state.settings, video: {resolution:newResolution}}})
+    }
+
+    render() { 
+        return (  
+        <div>
+            <button className='bitrate' onClick={this.changeBitrate}>{this.state.settings.bitrate}</button>
+            <button className='resolution' onClick={this.changeResolution}>{this.state.settings.video.resolution}</button>
+        </div>
+        );
+    }
+}
+ 
+export default YouTubeDeBugger;
